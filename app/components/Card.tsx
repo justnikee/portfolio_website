fb06d48dec9f363359d5da7fc2253119886f74ba
Nikhil <hk5561111212@gmail.com>
2025-05-25 22:14:11 +0530

    updated: work section

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const Card = ({title, description, image, stack, link}: Props) => {
   return (
-    <div id='card' className='bg-[#222220] rounded-lg border border-[#525252] flex-1 px-6 py-6'>
+    <div id='card' className='bg-[#222220] relative rounded-lg border border-[#525252] flex-1 px-6 py-6'>
       <div>
         <Link className='hoverTitle' href={link}>
         <h4 className='font-[Heading] text-3xl flex gap-3'>{title} <svg className='hoverIcon h-4 w-4 mt-1' xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 30 30" fill="none"><path fill="currentColor" d="M0 0h29.998L30 7.058 0 7.06V.001Z"></path><path fill="currentColor" d="M29.998 0 30 30h-7.06V0h7.058Z"></path><path fill="currentColor" d="M27.452 6.757 4.992 29.218 0 24.227l22.46-22.46 4.992 4.99Z"></path></svg></h4>
